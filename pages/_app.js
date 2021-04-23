10a488eede15876bc8633dc0d71fcacde0bee831
Eric Ma <me982@tamu.edu>
2021-04-22 20:25:41 -0500

    branch creation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout/layout'
+import axios from 'axios'
 
+axios.defaults.baseURL = "http://ericma.vercel.app"
 
 function MyApp({ Component, pageProps }) {
   return (

65a937bfe46e06ac731a3ad90704615a58e6c718
Eric Ma <me982@tamu.edu>
2021-04-22 20:34:02 -0500

    component name change

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout/layout'
+import Layout from '../components/Layout/Layout'
 import axios from 'axios'
 
 axios.defaults.baseURL = "http://ericma.vercel.app"

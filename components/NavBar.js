48391789f8ea4229722991bcfc384ee11eb55521
Eric Ma <me982@tamu.edu>
2021-04-25 23:56:46 -0500

    waiting for dns changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,7 +4,7 @@ export default function NavBar() {
     return (
         <nav className="flex items-center flex-wrap py-3 px-8 shadow-md">
             <Link href="/">
-                <span>Home</span>
+                <a>Home</a>
             </Link>
             <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
                 <div className="lg:flex-row lg:inline-flex lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Layout from '../components/Layout.js'
 import axios from 'axios'
 
-axios.defaults.baseURL = "https://www.ericswma.com"
+axios.defaults.baseURL = "http://ericma.vercel.app"
 
 function MyApp({ Component, pageProps }) {
   return (
